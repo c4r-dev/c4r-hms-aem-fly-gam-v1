36cc584b0f68c5a636f6c624bb0378e939c897c1
David Fischer <fiscd@seas.upenn.edu>
2024-08-09 10:01:52 -0400

    build app

--- app/page.js
+++ app/page.js
@@ -1,6 +1,35 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
+import Image from "next/image";
+import Raven1 from './assets/raven-1.svg';
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const startActivity = () => {
+    router.push(`/topicSelection`);
+  }
+
   return (
-    <div>HOME</div>
+    <>
+      <div>HOME</div>
+
+      <div className="activity-start-button-footer">
+        <button onClick={startActivity}>Start Activity</button>
+      </div>
+
+      <div>
+        <Image
+          priority
+          src={Raven1}
+          alt="Follow us at c4r.io"
+          className="background-image"
+        />
+      </div>
+    </>
   );
 }
